b6985b05d1fa39ed76f24eee577bbe81d93e06b8
Will Ely <rely@colgate.edu>
2015-03-31 19:27:38 -0400

    started some index stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+    def index
+
+
+
+
+
+
+
+
+
+
+
+private
+    def create_update_params
+        params.require(:product).permit(:name, :description, :price, :minimum_age_appropriate, :maximum_age_appropriate, :image)
+    end
 end
