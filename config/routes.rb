2a17b549c5bbeada8a5b0b9c5863cc43d9d482a0
danieldrucker <drucker.daniel@hotmail.com>
2015-04-01 21:08:30 -0400

    added Login using google omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     resources :products
-    resources :users
     root 'products#index'
     #root :to => redirect('/products')
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+    #devise_scope :user do
+    #    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    #end
 end
