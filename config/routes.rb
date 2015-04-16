db688e7568f0fb48f7fa6b046d02ff9e0925b60d
will199207 <wgarcia@colgate.edu>
2015-04-16 14:14:32 -0400

    search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
     resources :products
     root 'products#index'
-    #root :to => redirect('/products')
+    get 'search', to: 'products#search'
     devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
     get "/auth/:provider/callback" => 'sessions#create'
-    #devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-    #devise_scope :user do
-     #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-    #end
 end
