5773729ff715f6b62501466b82f12078f72ad852
will199207 <wgarcia@colgate.edu>
2015-04-20 20:52:22 -0400

    google oauth working

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 	    if registered_user
 		return registered_user
 	    else
-	        user = User.create(name: data["name"],
+	        user = User.create(first_name: data["name"],
 		provider:access_token.provider,
 		email: data["email"],
 		uid: access_token.uid ,
