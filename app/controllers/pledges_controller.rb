53d23b9d4920aee746c64ddc2bfceeb18623a300
will199207 <wgarcia@colgate.edu>
2015-04-30 16:59:08 -0400

    fixed the silent failure of strong params......

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,20 +1,25 @@
 class PledgesController < ApplicationController
-    def new
+    def new (values)
+    
     end
-
-
     def create
         @product = Product.find(params[:product_id])
         @product.pledge_count = @product.pledge_count + params[:pledge_num].to_i
         @product.save
-        @pledge = Pledge.new({:user_id => params[:user_id], :product_id => params[:product_id]})
-        if @pledge.save
-            flash[:notice] = "Your pledge went through!"
-            redirect_to product_path @pledge.product_id
-        else
-            flash[:alert] = "Pledge could not go through. Try again"
-            redirect_to product_path @pledge.product_id
-        end
+        params[:user_id] = current_user.id
+        params[:product_id] = @product.id.to_i
+        values = create_update_params
+        for i in 0..params[:pledge_num].to_i
+            @pledge = Pledge.new(values)
+            if not @pledge.save
+                flash[:alert] = "Pledge could not go through. Try again"
+                redirect_to product_path @pledge.product_id
+            end
+        end 
+
+        flash[:notice] = "Your pledge went through!"
+        redirect_to product_path @product
+
     end
 
     def show
@@ -30,7 +35,9 @@ class PledgesController < ApplicationController
 
 
     private
-    def pledge_update_params
-        params.require(:user_id).require(:product_id)
+    def create_update_params
+        params.require(:user_id)
+        params.require(:product_id)
+        params.permit(:user_id, :product_id)
     end
 end
